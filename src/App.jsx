33599364f6fdb3225ebb18dda7f5191f55b81a66
wakiullah <m.wakiullah1212@gmail.com>
2025-07-04 21:41:01 +0600

    add more things

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,17 @@ function App() {
   return (
     <>
       <div>
-      
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <div>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
