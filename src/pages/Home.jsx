94bf42fe851eb663688fbc0a37b640a9a8f6a0d3
wakiullah <m.wakiullah1212@gmail.com>
2025-07-10 19:17:29 +0600

    update home js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Hero from '../components/Hero'
 import Filter from '../components/filter';
 import Products from '../components/Products';
 import { CartContext } from '../Features/Cart/CartContext';
 
 function Home() {
-    const cart = useContext(CartContext)
-    // console.log(cart);
-
     return (
         <>
             <Hero />
